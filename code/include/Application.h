f65efdc05d588f48c08e31ec98e11f07a6008b73
zhentongdu@Ubuntu <zhengtongdu@zju.edu.cn>
2023-12-05 14:42:09 +0800

    feat: 现在是把所有原先在main.cpp中的代码都挪了出去,放在Application.cpp中了,下一步是对着LearnOpenGL里写过的内容完成一个简单的图形渲染

--- code/include/Application.h
+++ code/include/Application.h
@@ -1,8 +1,8 @@
 #ifndef APPLICATION_H
 #define APPLICATION_H
 
-#include "Renderer.h"
-#include "EventHandler.h"
+// #include "Renderer.h"
+// #include "EventHandler.h"
 
 class Application {
 public:
@@ -11,9 +11,10 @@ public:
     void Run();
 
 private:
-    Renderer renderer;
-    EventHandler eventHandler;
+    // Renderer renderer;
+    // EventHandler eventHandler;
     // 其他应用程序状态和逻辑
 };
+
 
 #endif // !APPLICATION_H
--- code/src/Application.cpp
+++ code/src/Application.cpp
@@ -1,4 +1,14 @@
 #include "Application.h"
+#include "imgui.h"
+#include "backends/imgui_impl_opengl3.h"
+#include "backends/imgui_impl_glfw.h"
+#include <GLFW/glfw3.h> // 包含glfw3.h
+#include <iostream>
+
+
+static void glfw_error_callback(int error, const char* description) {
+    fprintf(stderr, "Glfw Error %d: %s\n", error, description);
+}
 
 Application::Application() {
     // 初始化GLFW，设置OpenGL上下文和ImGui等
@@ -9,10 +19,67 @@ Application::~Application() {
 }
 
 void Application::Run() {
-    while (!eventHandler.ShouldClose()) {
-        eventHandler.PollEvents();
-        renderer.BeginFrame();
-        // GUI代码
-        renderer.EndFrame();
+    // 设置错误回调
+    glfwSetErrorCallback(glfw_error_callback);
+    
+    // 初始化GLFW
+    if (!glfwInit())
+        return ;
+
+    // 创建窗口
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "MyGuiApp", NULL, NULL);
+    if (window == NULL)
+        return ;
+
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1); // 启用垂直同步
+
+    // 初始化ImGui
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+
+    // 设置ImGui样式
+    ImGui::StyleColorsClassic();
+
+    // 绑定ImGui与GLFW
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 130");
+
+    // 主循环
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+
+        // 开始ImGui帧
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        // 渲染你的GUI
+        ImGui::Begin("Hello, world!");
+        ImGui::Text("This is some useful text.");
+        // ... 更多的GUI代码
+        ImGui::End();
+
+        // 渲染ImGui
+        ImGui::Render();
+        int display_w, display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
+        glClearColor(0.45f, 0.55f, 0.60f, 1.00f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        // 交换缓冲区
+        glfwSwapBuffers(window);
     }
+
+    // 清理
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
 }
